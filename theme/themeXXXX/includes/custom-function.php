7a936f010e843853b79266cf9fdcd9bb98d8fae6
alexey-svistunov <alexeysvistunov@gmail.com>
2014-06-30 11:40:00 +0300

    Init

---
+++ theme/themeXXXX/includes/custom-function.php
@@ -0,0 +1,7 @@
+<?php
+	// Loads child theme textdomain
+	load_child_theme_textdomain( CURRENT_THEME, CHILD_DIR . '/languages' );
+
+	// Loads custom scripts.
+	// require_once( 'custom-js.php' );
+?>
---
+++ theme/themeXXXX/includes/custom-js.php
@@ -0,0 +1,10 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'cherry_child_custom_scripts' );
+
+function cherry_child_custom_scripts() {
+	/**
+	 * How to enqueue script?
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/wp_enqueue_script
+	 */
+} ?>
